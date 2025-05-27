89e1f6249908eab4eb090a7e1c3dca0e8c29e4ee
Shashank <116636799+Github-Shashank@users.noreply.github.com>
2025-05-27 22:58:28 +0530

    Using the Math Module for Calculations

---
+++ Assignment_3-Task_2.py
@@ -0,0 +1,13 @@
+# Using the Math Module for Calculations
+
+import math
+
+num_1 = int(input("Enter a number: "))
+
+sqrt = math.sqrt(num_1)
+log  = math.log(num_1)
+sin  = math.sin(num_1)
+
+print(f"Square root: {sqrt}")
+print(f"Logarithm: {log}")
+print(f"Sine: {sin}")
